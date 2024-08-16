771c8337941f10084a4d58889c5be75b5ba56234
Brandon <brandon.silva.180@gmail.com>
2024-08-16 00:13:18 -0600

    prueba2

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
-@echo off
-REM Actualizar pip
+#!/bin/bash
+# Actualizar pip
 python -m pip install --upgrade pip
 
-REM Instalar dependencias del proyecto
+# Instalar dependencias del proyecto
 pip install -r requirements.txt
